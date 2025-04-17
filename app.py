a9fc1f205f9527492a32fc95ed1e9a468eaa2997
RHHUELSMANN <rsbhuelsmann@gmail.com>
2025-04-17 16:17:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import os
 import re
 import textwrap
 import difflib
-
-
 import streamlit as st
 import pandas as pd
 import fitz  # PyMuPDF
 from openai import OpenAI
+from word_styling import export_doc
+from datetime import datetime
+
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def pdf_suche(pfad, suchbegriff):
@@ -100,13 +101,50 @@ def bereinige_export_daten(daten):
             export_daten[key] = value
     return export_daten
 
-from word_styling import export_doc
-
 with st.form("eingabeformular"):
     name = st.text_input("Kundenname")
     zielgebiet = st.radio("Zielgebiet", ["Europa", "Welt"], index=0)
     preis = st.number_input("Reisepreis (€)", min_value=0.0)
     alter_text = st.text_input("Alter (z. B. 45 48)")
+
+col1, col2, col3, col4 = st.columns(4)
+gb_eingaben = []
+
+with col1:
+    gb_eingaben.append(st.text_input("", key="gb1", label_visibility="collapsed"))
+with col2:
+    gb_eingaben.append(st.text_input("", key="gb2", label_visibility="collapsed"))
+with col3:
+    gb_eingaben.append(st.text_input("", key="gb3", label_visibility="collapsed"))
+with col4:
+    gb_eingaben.append(st.text_input("", key="gb4", label_visibility="collapsed"))
+
+def parse_geburtstag(text):
+    try:
+        text = text.strip().replace(" ", "").replace(".", "")
+        if len(text) == 6:
+            return datetime.strptime(text, "%d%m%y").date()
+        elif len(text) == 8:
+            return datetime.strptime(text, "%d%m%Y").date()
+        elif "." in text and len(text.split(".")[-1]) == 2:
+            return datetime.strptime(text, "%d.%m.%y").date()
+        elif "." in text:
+            return datetime.strptime(text, "%d.%m.%Y").date()
+    except:
+        return None
+
+heute = date.today()
+geburts_alter = []
+
+for geb in gb_eingaben:
+    gebdat = parse_geburtstag(geb)
+    if gebdat:
+        alter = heute.year - gebdat.year - ((heute.month, heute.day) < (gebdat.month, gebdat.day))
+        geburts_alter.append(alter)
+
+# automatisch als Text in "alter_text" schreiben, wenn vorhanden
+if geburts_alter:
+    alter_text = " ".join(str(a) for a in geburts_alter)
     von_raw = st.text_input("Reise von (TTMM oder TT.MM.JJJJ)")
     bis_raw = st.text_input("Reise bis (TTMM oder TT.MM.JJJJ)")
     submit = st.form_submit_button("Tarife anzeigen")
