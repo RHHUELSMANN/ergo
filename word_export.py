5e12086af285cae5d6989ca1eb22c6a5243145e5
RHHUELSMANN <rsbhuelsmann@gmail.com>
2025-05-09 15:47:03 +0200

    Update word_export.py

--- word_export.py
+++ word_export.py
@@ -1,10 +1,16 @@
-
 import streamlit as st
 import tempfile
 from word_styling import export_doc
 
+def berechne_betrag(p, reisepreis):
+    return p * reisepreis if p < 1 else p
+
 def euro_wert(wert):
     return f"{float(wert):,.2f}".replace(".", "X").replace(",", ".").replace("X", ",") + " €"
+
+def euro_betrag(p, reisepreis):
+    betrag = berechne_betrag(p, reisepreis)
+    return euro_wert(betrag)
 
 def export_word_dokument(name, reisepreis, reise_von, reise_bis, zielgebiet, alter_liste, max_alter,
                          rrv_ev_mit, rrv_ev_ohne,
@@ -23,22 +29,22 @@ def export_word_dokument(name, reisepreis, reise_von, reise_bis, zielgebiet, alt
         "Alter": str(max_alter),
         "Reiseziel": zielgebiet,
 
-        "Reiseruecktritt_Einmal_mit_SB": euro_wert(rrv_ev_mit),
-        "Reiseruecktritt_Einmal_ohne_SB": euro_wert(rrv_ev_ohne),
-        "Reiseruecktritt_Jahres_mit_SB": euro_wert(rrv_jv_mit),
-        "Reiseruecktritt_Jahres_ohne_SB": euro_wert(rrv_jv_ohne),
-        "Reiseruecktritt_Jahres_Sparfuchs_mit_SB": euro_wert(spf_mit),
-        "Reiseruecktritt_Jahres_Sparfuchs_ohne_SB": euro_wert(spf_ohne),
+        "Reiseruecktritt_Einmal_mit_SB": euro_betrag(rrv_ev_mit, reisepreis),
+        "Reiseruecktritt_Einmal_ohne_SB": euro_betrag(rrv_ev_ohne, reisepreis),
+        "Reiseruecktritt_Jahres_mit_SB": euro_betrag(rrv_jv_mit, reisepreis),
+        "Reiseruecktritt_Jahres_ohne_SB": euro_betrag(rrv_jv_ohne, reisepreis),
+        "Reiseruecktritt_Jahres_Sparfuchs_mit_SB": euro_betrag(spf_mit, reisepreis),
+        "Reiseruecktritt_Jahres_Sparfuchs_ohne_SB": euro_betrag(spf_ohne, reisepreis),
 
-        "Reisekranken_Einmal_mit_SB": euro_wert(kranken_mit),
-        "Reisekranken_Einmal_ohne_SB": euro_wert(kranken_ohne),
-        "Reisekranken_Jahres_mit_SB": euro_wert(kv_jv_mit),
-        "Reisekranken_Jahres_ohne_SB": euro_wert(kv_jv_ohne),
+        "Reisekranken_Einmal_mit_SB": euro_betrag(kranken_mit, reisepreis),
+        "Reisekranken_Einmal_ohne_SB": euro_betrag(kranken_ohne, reisepreis),
+        "Reisekranken_Jahres_mit_SB": euro_betrag(kv_jv_mit, reisepreis),
+        "Reisekranken_Jahres_ohne_SB": euro_betrag(kv_jv_ohne, reisepreis),
 
-        "RundumSorglos_Einmal_mit_SB": euro_wert(rus_ev_mit),
-        "RundumSorglos_Einmal_ohne_SB": euro_wert(rus_ev_ohne),
-        "RundumSorglos_Jahres_mit_SB": euro_wert(rus_jv_mit),
-        "RundumSorglos_Jahres_ohne_SB": euro_wert(rus_jv_ohne),
+        "RundumSorglos_Einmal_mit_SB": euro_betrag(rus_ev_mit, reisepreis),
+        "RundumSorglos_Einmal_ohne_SB": euro_betrag(rus_ev_ohne, reisepreis),
+        "RundumSorglos_Jahres_mit_SB": euro_betrag(rus_jv_mit, reisepreis),
+        "RundumSorglos_Jahres_ohne_SB": euro_betrag(rus_jv_ohne, reisepreis),
     }
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as tmp:
